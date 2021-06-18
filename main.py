d37b4c207700152f212c70010580dac2456d49e1
Razi <razialisalehlol@gmail.com>
2021-06-18 15:16:59 +0300

    query now has a loop that fetches weekly data

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import twint
+import nest_asyncio
+from datetime import date,timedelta
+
+from query import gettweets
+print(date(2021,6,13).strftime("%Y-%m-%d"))
+end = date(2021,6,18)
+for i in range (52):
+    start = end - timedelta(days=7)
+    gettweets(start.strftime("%Y-%m-%d"),end.strftime("%Y-%m-%d"),"btc OR bitcoin")
+    end -= timedelta(days=7)
+
+
+
+
