5e3ba577f8c381c708c04bf1883cc1bf82b7d300
Razi <razialisalehlol@gmail.com>
2021-06-18 14:08:48 +0300

    cleaning up query (getting rid of useless data)

--- query.py
+++ query.py
@@ -5,6 +5,8 @@ def gettweets(fromdate,todate,keywords):
     """This is coded to only work on Razi's pcs for now"""
     """TODO: make the path more abstract"""
     c = twint.Config()
+    c.Custom["tweet"] = ["date","time","username","tweet","language","replies_count","retweets_count",
+                         "likes_count","hashtags","cashtags"]
     c.Search = keywords
     nest_asyncio.apply()
     c.Store_csv = True
