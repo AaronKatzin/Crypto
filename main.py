4af885c83d85d69010ffac9364744e48c1909e71
akatzin <aaron.katzin@intel.com>
2021-06-22 13:47:21 +0300

    updates main to be cleaner and use all helper functions

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import twint,os
 import nest_asyncio
 import pandas as pd
 from datetime import date, timedelta
-from getters import getHashtags, getStats, getTweetStatsDF
+from getters import getHashtags, getStats, getTweetStatsDF, getAllFilesStats,getExtendedStatsDF
+from cleaning import cleanDF
 from query import gettweets
 import numpy as np
 from tabulate import tabulate
@@ -13,85 +14,34 @@ import matplotlib.pyplot as plt
 from decisionTree import renderTree
 import graphviz
 from sklearn.ensemble import RandomForestClassifier
-from predict import predict
+from predict import predict,predictList
 
 MAJOR_CHANGE = 5
 
-"""print(date(2021,6,13).strftime("%Y-%m-%d"))
-end = date(2021,6,18)
-for i in range (52):
-    start = end - timedelta(days=7)
-    gettweets(start.strftime("%Y-%m-%d"),end.strftime("%Y-%m-%d"),"btc OR bitcoin")
-    end -= timedelta(days=7)"""
-
-# getHashtags("data\\2021-06-11-2021-06-18btc OR bitcoin.csv")
-"""getStats("data\\2021-04-23-2021-04-30btc OR bitcoin.csv")
-getStats("data\\2021-04-30-2021-05-07btc OR bitcoin.csv")
-getStats("data\\2021-05-07-2021-05-14btc OR bitcoin.csv")
-getStats("data\\2021-05-14-2021-05-21btc OR bitcoin.csv")
-getStats("data\\2021-05-21-2021-05-28btc OR bitcoin.csv")
-getStats("data\\2021-05-28-2021-06-04btc OR bitcoin.csv")
-getStats("data\\2021-06-04-2021-06-11btc OR bitcoin.csv")
-getStats("data\\2021-06-11-2021-06-18btc OR bitcoin.csv")"""
+#only needed when there's new data
+#getAllFilesStats()
 
 statsDF = getTweetStatsDF()
 coinPriceDF = pd.read_csv("data\\BTC-USD.csv")
 
 df = statsDF.set_index('date').join(coinPriceDF.set_index('date'))
 
-df['increase_open'] = np.where(df['Open'].shift(1) < df['Open'], 1, 0)
-df['percent_change'] = (df['Open'] - df['Open'].shift(1)) / df['Open'] * 100
-df['major_increase'] = np.where(df['percent_change'] > MAJOR_CHANGE, 1, 0)
-df['major_decrease'] = np.where(df['percent_change'] < - MAJOR_CHANGE, 1, 0)
-df['major_change'] = np.where(df['percent_change'] > MAJOR_CHANGE, 1, 0) + np.where(df['percent_change'] < - MAJOR_CHANGE, 1, 0)
+df = getExtendedStatsDF(df, MAJOR_CHANGE)
 
-#print(tabulate(df, headers='keys', tablefmt='psql'))
+
+
+df = cleanDF(df)
+
+
+print(tabulate(df, headers='keys', tablefmt='psql'))
 
 used_features = ['tweet_count','replies','avg_replies','retweets','average_retweets','likes','average_likes',
-                 'diamondhands_count','investing_count','hodl_count','hold_count','tesla_count','sell_count','buy_count',
-                 'elon_musk_count','shorttesla_count','referral_count','gold_count','moon_count']
+                     'diamondhands_count','investing_count','hodl_count','hold_count','tesla_count','sell_count',
+                     'buy_count','elon_musk_count','shorttesla_count','referral_count','gold_count','moon_count',
+                     "diamondhands_count_avg","investing_count_avg","hodl_count_avg","hold_count_avg","tesla_count_avg",
+                     "sell_count_avg","buy_count_avg","elon_musk_count_avg","shorttesla_count_avg","referral_count_avg",
+                     "gold_count_avg","moon_count_avg"]
 
-predict(df, used_features, "major_increase")
-predict(df, used_features, "major_decrease")
-predict(df, used_features, "major_change")
-
-"""
-used_features = ['tweet_count','replies','avg_replies','retweets','average_retweets','likes','average_likes',
-                 'diamondhands_count','investing_count','hodl_count','hold_count','tesla_count','sell_count','buy_count',
-                 'elon_musk_count','shorttesla_count','referral_count','gold_count','moon_count']"""
-"""print("Decision tree:")
-XTrain, XTest, YTrain, YTest = train_test_split(df[used_features].values, df['major_change'].values, random_state=1, test_size=0.2)
-max = 0
-
-
-decisionTree = DecisionTreeClassifier(max_depth=2, min_samples_split=10)
-decisionTree = decisionTree.fit(XTrain, YTrain)
-
-y_pred_train = decisionTree.predict(XTrain)
-#print("Split: " + str(split) + ". depth: " + str(depth) + ". Accuracy on training data = ", metrics.accuracy_score(y_true = YTrain, y_pred = y_pred_train))
-
-y_pred  = decisionTree.predict(XTest)
-accuracy = metrics.accuracy_score(y_true = YTest, y_pred = y_pred)
-print(" Accuracy on test data = ", accuracy)
-
-
-print("max: " + str(max))
-
-text_representation = export_text(decisionTree)
-print(text_representation)
-
-print("\nRandom forest")
-
-
-
-forest =  RandomForestClassifier(bootstrap=True, n_estimators=300, random_state=0)
-
-trained_forest = forest.fit(XTrain, YTrain)
-
-y_pred_train = trained_forest.predict(XTrain)
-print('Accuracy on training data= ', metrics.accuracy_score(y_true = YTrain, y_pred = y_pred_train))
-
-y_pred = trained_forest.predict(XTest)
-print('Accuracy on test data= ', metrics.accuracy_score(y_true = YTest, y_pred = y_pred))
-"""
-"""print(statsDF.join(coinPriceDF, on = 'date'))"""
+category_list = ["increase","decrease","major_increase","major_decrease","major_change","increase_tomorrow",
+                 "decrease_tomorrow","major_increase_tomorrow","major_decrease_tomorrow","major_change_tomorrow"]
+predictList(df, used_features,category_list)
