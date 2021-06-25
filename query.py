2a9345174d40f63bcd58a78b99f6a897a98d8431
Razi <razialisalehlol@gmail.com>
2021-06-25 12:54:15 +0300

    CLEANUP: adds comment to query.py and visualization.py

--- query.py
+++ query.py
@@ -7,7 +7,20 @@ import pandas as pd
 
 
 def gettweets(fromdate, todate, keywords):
+    """
+        gettweets(fromdate, todate, keywords)
+
+        Farms and collects tweets from and to a specific date that contain specific hashtags/keywords
+
+        arguments:
+        fromdate: the lower date bound that we want to collect our data from
+        todate: the upper date bound that we want to collect our data from
+        keywords: specific hashtags/keywords that the tweet must include for it to be saved
+
+        :returns saves the tweets in csv files
+        """
     c = twint.Config()
+    #specific data we want to save alongside with our tweet
     c.Custom["tweet"] = ["date", "time", "username", "tweet", "language", "replies_count", "retweets_count",
                          "likes_count", "hashtags", "cashtags"]
     c.Search = keywords
@@ -17,11 +30,24 @@ def gettweets(fromdate, todate, keywords):
     c.Until = todate
     c.Hide_output = True
     current_dir = os.path.dirname(os.path.realpath(__file__))
+    #the file is named neatly to represent the dates of the tweets it includes and the keywords that were searched for
     c.Output = (current_dir + "\\Data\\" + fromdate + "-" + todate + keywords + ".csv")
     twint.run.Search(c)
 
 
 def getWeeksOfTweets(endYear, endMonth, endDay, weeksToGet, keywords):
+    """
+            getWeeksOfTweets(endYear, endMonth, endDay, weeksToGet, keywords)
+
+            uses gettweets to farm a specific amount of weeks worth of tweets
+
+            arguments:
+            endYear: the upper year bound that we want to collect our data from
+            endMonth: the upper month bound that we want to collect our data from
+            endDay: the upper day bound that we want to collect our data from
+            weeksToGet: amount of weeks worth of tweets we want to farm
+            keywords: specific hashtags/keywords that the tweet must include for it to be saved
+            """
     end = date(endYear, endMonth, endDay)
     for i in range(weeksToGet):
         start = end - timedelta(days=7)
@@ -30,11 +56,23 @@ def getWeeksOfTweets(endYear, endMonth, endDay, weeksToGet, keywords):
 
 
 def crawlSandP():
+    """
+                crawlSandP()
+
+                crawls the SPAVG data from https://stooq.com (https://stooq.com/q/d/?s=%5Espx&c=0 specifically)
+
+                arguments:
+                none
+
+                :returns saves the crawled data in a csv file
+                """
+
     baseURL = "https://stooq.com/q/d/?s=%5Espx&c=0"
+    #the time period we wanted to crawl our data from
     fromDate = "20200619"
     toDate = "20210619"
     url = baseURL + "&d1=" + fromDate + "&d2=" + toDate
-
+    #initializing arrays we want to save our wanted data in
     date = []
     dailyAvg = []
     for page in range(1, 7):
@@ -43,16 +81,18 @@ def crawlSandP():
         table = soup("table", attrs={"class": "fth1"})[0]
         first = True
         for row in table("tr"):
+            #The first row doesn't interest us so we're skipping it using the boolean "first"
             if first:
                 first = False
             else:
+                #crawling and appending the data we're interested in into arrays
                 cells = row("td")
                 date_object = datetime.strptime(cells[1].get_text(), "%d %b %Y")
                 date.append(date_object.strftime("%Y-%m-%d"))
                 print("Date: " + date_object.strftime("%Y-%m-%d"))
                 dailyAvg.append((float(cells[3].get_text()) + float(cells[4].get_text()))/2)
                 print("daily avg: ", ((float(cells[3].get_text()) + float(cells[4].get_text()))/2))
-
+    #saving the data we crawled into a csv file
     current_dir = os.path.dirname(os.path.realpath(__file__))
     outputFile = open(current_dir + "\\data\\" + fromDate + "_" + toDate + "_" + "S&P_Daily_avg.csv", 'w', newline='')
     writer = csv.writer(outputFile)
@@ -69,6 +109,16 @@ def crawlSandP():
 
 
 def getSoup(url):
+    """
+                    getSoup(url)
+
+                    creates and returns the soup object
+
+                    arguments:
+                    URL that we want to turn into the soup object
+
+                    :returns returns a soup object corresponding to the given URL
+                    """
     user_agent = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=user_agent)
     return BeautifulSoup(response.content, "html.parser")
--- visualization.py
+++ visualization.py
@@ -11,46 +11,65 @@ from tabulate import tabulate
 import matplotlib.pyplot as plt
 
 def normalize(df, column_name):
+    """
+        normalize(df, column_name)
+        Normalizes the data frame to make it visually easier to display and understand in charts
 
+        arguments:
+        df:the data frame we want to normalize
+
+        column_name: the column we want to normalize
+
+        :returns a dataframe with an added normalized column to be displayed visually
+        """
+    #Calculates the normal using the max and the min values
     max_value = df[column_name].max()
     min_value = df[column_name].min()
+    #Adds a new column that includes the normalized values
     df[column_name + "_normalized"] = (df[column_name] - min_value) / (max_value - min_value)
     return df
 
-from predict import predict,predictList
 
+#We've set our major change to be at 5%
 MAJOR_CHANGE=5
 warnings.filterwarnings("ignore")
 
+#fills a data frame with the tweet stats using getTweetStatsDF
 statsDF = getTweetStatsDF()
+#fills a data frame with the coin price stats using a csv file that contains them
 coinPriceDF = pd.read_csv("data\\BTC-USD.csv")
+#fills a data frame with the SPAVG price stats using a csv file that contains them
 SPAvgDF = pd.read_csv("data\\20200619_20210619_S&P_Daily_avg.csv")
-
+#syncronizes the way the date column is displayed to avoid errors
 statsDF['datetime'] = pd.to_datetime(statsDF['date'],format="%Y-%m-%d", errors='ignore' )
+#joins the coinprice and the SPavgDF data frames into one dataframe depending on the date column
 df = statsDF.set_index('date').join(coinPriceDF.set_index('date'))
 df = df.join(SPAvgDF.set_index('date'))
 
 
-
-#print(tabulate(df, headers='keys', tablefmt='psql'))
-
+#adds additional columns to the data frame(e.g. averages)
 df = getExtendedStatsDF(df, MAJOR_CHANGE)
 
 
-
+#cleans the data frame (removes NULL values and fills in the SPAVG weekend values)
 df = cleanDF(df)
 
+#list of columns that we want to normalize for the visualization
 toNormalize = ["diamondhands_count_avg","sell_count_avg","tesla_count_avg","SPAvg","gold_count",
                "elon_musk_count_avg","buy_count_avg","hodl_count","elon_musk_count","referral_count_avg","buy_count"]
 
+#normalizing each column
 for column in toNormalize:
     normalize(df, column)
 normalize(df, "BTCAvg")
-
+#prints the dataframe
 print(tabulate(df, headers='keys', tablefmt='psql'))
 
+#normalized columns to visualize
 normalized = ["diamondhands_count_avg_normalized","sell_count_avg_normalized","tesla_count_avg_normalized","SPAvg_normalized","gold_count_normalized",
                "elon_musk_count_avg_normalized","buy_count_avg_normalized","hodl_count_normalized","elon_musk_count_normalized","referral_count_avg_normalized","buy_count_normalized"]
+
+#plots the normalized data for us to study
 for column in normalized:
     g_results = sns.lineplot(data=df[["datetime",column, "BTCAvg_normalized"]])
     plt.show()
