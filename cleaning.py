9eec8a7ef1b8be98e62130bb43f5b5c1cfb7c992
akatzin <aaron.katzin@intel.com>
2021-06-25 11:45:59 +0300

    CLEANUP: adds comments to cleaning.py and predict.py

--- cleaning.py
+++ cleaning.py
@@ -1,15 +1,27 @@
-import pandas as pd
-
 def cleanDF(df):
-    # clean S&P weekend null values
+    """
+    cleanDF(df)
+    cleans the dataframe of NULLS by filling S&P values and deleting other rows with NULLs
+
+    arguments:
+    df: a pandas dataframe to be cleaned
+
+    :returns a cleaned dataframe
+    """
+    # fill S&P weekend null values
     df["SPAvg"] = df["SPAvg"].fillna(method='ffill', inplace=False)
 
-    #clean nulls
+    # clean nulls
+    # print datafram before cleaning
     print("Dataframe shape before cleaning: " + str(df.shape))
-    null_columns=df.columns[df.isnull().any()]
+    null_columns = df.columns[df.isnull().any()]
     print("null columns before cleaning: ", df[null_columns].isnull().sum())
-    df.dropna(axis=0, how='any',inplace=True)
-    null_columns=df.columns[df.isnull().any()]
+
+    # drop NULLs
+    df.dropna(axis=0, how='any', inplace=True)
+
+    # print datafram after dropping NULLs
+    null_columns = df.columns[df.isnull().any()]
     print("Cleaned Dataframe shape: " + str(df.shape))
     print("After cleaning null columns: ", df[null_columns].isnull().sum())
 
--- predict.py
+++ predict.py
@@ -1,31 +1,62 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics as metrics
-from sklearn.tree import export_graphviz, export_text
+from sklearn.tree import export_text
 
 
-def predict(df, used_features, catagory):
-    XTrain, XTest, YTrain, YTest = train_test_split(df[used_features].values, df[catagory].values, random_state=1,
+def predict(df, used_features, category):
+    """
+    predict(df, used_features, category)
+    trains and tests the random forest algorithm/model based on the given features and requested categories
+
+    arguments:
+    df: a pandas dataframe containing our data
+    used_features: a list of features to use in the predictions
+    category: the category to predict
+
+    :returns nothing. prints results to console
+    """
+
+    # split data into test and train
+    XTrain, XTest, YTrain, YTest = train_test_split(df[used_features].values, df[category].values, random_state=1,
                                                     test_size=0.5)
 
-    print("\nRandom forest for catagory: " + catagory)
+    print("\nRandom forest for category: " + category)
 
+    # creating the classifier
     forest = RandomForestClassifier(bootstrap=True, n_estimators=300, random_state=0)
 
+    # train the model
     trained_forest = forest.fit(XTrain, YTrain)
 
+    # predict on training data
     y_pred_train = trained_forest.predict(XTrain)
     print('Accuracy on training data= ', metrics.accuracy_score(y_true=YTrain, y_pred=y_pred_train))
 
+    # predict on test data
     y_pred = trained_forest.predict(XTest)
     print('Accuracy on test data= ', metrics.accuracy_score(y_true=YTest, y_pred=y_pred))
 
+    # print the tree
     estimator = forest.estimators_[5]
     print(export_text(estimator,
                     feature_names=used_features))
 
 
 def predictList(df, used_features,category_list):
+    """
+    predictList(df, used_features, category)
+    calls the predict function for each category in the given list
+
+    arguments:
+    df: a pandas dataframe containing our data
+    used_features: a list of features to use in the predictions
+    category: a list of categories to predict
+
+    :returns nothing. prints results to console
+    """
+    # iterate over categories
     for category in category_list:
+        # call helper function
         predict(df, used_features, category)
 
