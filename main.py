470acab4a0d85d53c86fb4f7376eaf381ab32492
akatzin <aaron.katzin@intel.com>
2021-06-25 10:08:25 +0300

    CLEANUP: remove unnecessary imports and add comments to main

--- main.py
+++ main.py
@@ -1,60 +1,60 @@
-import twint,os
-import nest_asyncio
 import pandas as pd
-from datetime import date, timedelta
-from getters import getHashtags, getStats, getTweetStatsDF, getAllFilesStats,getExtendedStatsDF
+from getters import getHashtags, getStats, getTweetStatsDF, getAllFilesStats, getExtendedStatsDF
 from cleaning import cleanDF
-from query import gettweets,getWeeksOfTweets
-import numpy as np
 from tabulate import tabulate
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text
-from sklearn import metrics as metrics
-import matplotlib.pyplot as plt
-from decisionTree import renderTree
-import graphviz
-from sklearn.ensemble import RandomForestClassifier
-from predict import predict,predictList
+from predict import predict, predictList
 
+# setting major change percentage
 MAJOR_CHANGE = 5
 
-#to get new tweet data
-#getWeeksOfTweets(2020,4,16,52, "btc OR bitcoin")
+# to get new tweet data, only needs to run once
+# getWeeksOfTweets(2020,4,16,52, "btc OR bitcoin")
 
-#only needed when there's new data
-#getAllFilesStats()
+# parse raw data to create daily stats files, only needed when there's new data
+# getAllFilesStats()
 
+# load daily stats files into a dataframe
 statsDF = getTweetStatsDF()
+
+# load daily coin price file into a dataframe
 coinPriceDF = pd.read_csv("data\\BTC-USD.csv")
+
+# load daily S&P 500 index file into a dataframe
 SPAvgDF = pd.read_csv("data\\20200619_20210619_S&P_Daily_avg.csv")
 
+# join daily stats files drataframe with the coin price and
 df = statsDF.set_index('date').join(coinPriceDF.set_index('date'))
 df = df.join(SPAvgDF.set_index('date'))
 
+# print dataframe for debugging
+# print(tabulate(df, headers='keys', tablefmt='psql'))
 
-
-#print(tabulate(df, headers='keys', tablefmt='psql'))
-
+# calculate additional stats columns (e.g. averages) and add them to the dataframe
 df = getExtendedStatsDF(df, MAJOR_CHANGE)
 
-
-
+# clean the dataframe of missing data, NULLs, etc
 df = cleanDF(df)
 
-
+# print dataframe for debugging
 print(tabulate(df, headers='keys', tablefmt='psql'))
 
-used_features = ['tweet_count','replies','avg_replies','retweets','average_retweets','likes','average_likes',
-                     'diamondhands_count','investing_count','hodl_count','hold_count','tesla_count','sell_count',
-                     'buy_count','elon_musk_count','shorttesla_count','referral_count','gold_count','moon_count',
-                     "diamondhands_count_avg","investing_count_avg","hodl_count_avg","hold_count_avg","tesla_count_avg",
-                     "sell_count_avg","buy_count_avg","elon_musk_count_avg","shorttesla_count_avg","referral_count_avg",
-                     "gold_count_avg","moon_count_avg"]
-used_features_avgs_only = ['tweet_count','avg_replies','average_retweets','average_likes',
-                     "diamondhands_count_avg","investing_count_avg","hodl_count_avg","hold_count_avg","tesla_count_avg",
-                     "sell_count_avg","buy_count_avg","elon_musk_count_avg","shorttesla_count_avg","referral_count_avg",
-                     "gold_count_avg","moon_count_avg"]
+# choose features to be used in the ML algorithm
+used_features = ['tweet_count', 'replies', 'avg_replies', 'retweets', 'average_retweets', 'likes', 'average_likes',
+                 'diamondhands_count', 'investing_count', 'hodl_count', 'hold_count', 'tesla_count', 'sell_count',
+                 'buy_count', 'elon_musk_count', 'shorttesla_count', 'referral_count', 'gold_count', 'moon_count',
+                 "diamondhands_count_avg", "investing_count_avg", "hodl_count_avg", "hold_count_avg", "tesla_count_avg",
+                 "sell_count_avg", "buy_count_avg", "elon_musk_count_avg", "shorttesla_count_avg", "referral_count_avg",
+                 "gold_count_avg", "moon_count_avg", "SPAvg"]
+used_features_avgs_only = ['tweet_count', 'avg_replies', 'average_retweets', 'average_likes',
+                           "diamondhands_count_avg", "investing_count_avg", "hodl_count_avg", "hold_count_avg",
+                           "tesla_count_avg",
+                           "sell_count_avg", "buy_count_avg", "elon_musk_count_avg", "shorttesla_count_avg",
+                           "referral_count_avg",
+                           "gold_count_avg", "moon_count_avg"]
 
-category_list = ["increase","decrease","major_increase","major_decrease","major_change","increase_tomorrow",
-                 "decrease_tomorrow","major_increase_tomorrow","major_decrease_tomorrow","major_change_tomorrow"]
-predictList(df, used_features_avgs_only,category_list)
+# choose categories we would like to predict
+category_list = ["increase", "decrease", "major_increase", "major_decrease", "major_change", "increase_tomorrow",
+                 "decrease_tomorrow", "major_increase_tomorrow", "major_decrease_tomorrow", "major_change_tomorrow"]
+
+# train and test the model
+predictList(df, used_features, category_list)
